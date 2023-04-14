f42163a6848cf3a242dbbd90f80e4f7515136bcd
h21tisto <89834645+h21tisto@users.noreply.github.com>
2023-04-14 14:09:02 +0200

    Update MainClass.cs

--- Lab1/MainClass.cs
+++ Lab1/MainClass.cs
@@ -7,25 +7,30 @@
             ListReferencedBased<Person> p = new ListReferencedBased<Person>();
 
             Console.WriteLine($"Listan innehåller nu: {p.Length()} noder\n");
+
             // create people by filling in name, id and occupation
             p.Add(new Person("Benny", 100, "Bagare"));
             p.Add(new Person("Sören", 101, "Snickare"));
             p.Add(new Person("Maria", 102, "Maskinist"));
             p.Add(new Person("Roger", 103, "Universitetadjunkt - Informatik"));
+
             // returns a bool if true or false and the the list´s length
             Console.WriteLine($"\nisEmpty? - {p.IsEmpty()}");
             Console.WriteLine($"Listan innehåller nu: {p.Length()} noder\n");
+
             // write the person on index 2 to compare later
             p.Add(new Person("Johan", 104, "Professor - Mikrodataanalys"), 2);
             Console.WriteLine($"Person på index 2 är: {p.GetValueAtIndex(2)}\n");
             Console.WriteLine($"Listan innehåller nu: {p.Length()} noder\n");
-            // romove on index 2 and print the list again
+
+            // remove on index 2 and print the list again
             p.Remove(2);
             Console.WriteLine($"Listan innehåller nu: {p.Length()} noder\n");
             Console.WriteLine($"Index 1: {p.GetValueAtIndex(1)}");
             Console.WriteLine($"Index 2: {p.GetValueAtIndex(2)}");
             Console.WriteLine($"Index 3: {p.GetValueAtIndex(3)}");
             Console.WriteLine($"Index 4: {p.GetValueAtIndex(4)}");
+
             // doublecheck so values match
             Console.WriteLine($"\nPerson på index 2 är: {p.GetValueAtIndex(2)}\n");
             
@@ -33,6 +38,7 @@
             p.Clear();
             Console.WriteLine($"Kontrollerar: isEmpty? - {p.IsEmpty()}");
             Console.WriteLine($"Listan innehåller nu: {p.Length()} noder\n");
+
             // testing out of range index value
             Console.WriteLine("Provar att söka ut Person på index 10");
 
