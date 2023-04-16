9e670a505b01b83dd7097c10fb5b6a9df946e8a5
h21tisto <89834645+h21tisto@users.noreply.github.com>
2023-04-16 20:29:56 +0200

    Update Student.cs

--- Lab2/Lab2/Student.cs
+++ Lab2/Lab2/Student.cs
@@ -9,32 +9,33 @@ namespace Lab2
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Id { get; }
+        public int ID { get; }
         public string StudentID { get; set; }
 
 
         public Student(string FirstName, string LastName)
         {
-            Id = _counter++;
+            ID = _counter++;
             this.FirstName = FirstName;
             this.LastName = LastName;
-            GetStudentID();
+            StudentID = GetStudentID();
         }
 
         public override string ToString()
         {
-            return $"Id: {Id}, FirstName: {FirstName}, LastName: {LastName}, StudentID: {StudentID}";
+            return $"Id: {ID}, FirstName: {FirstName}, LastName: {LastName}, StudentID: {StudentID}";
         }
 
-        private void GetStudentID()
+        private string GetStudentID()
         {
-            string sid = string.Empty;
+            string sID = string.Empty;
             DateTime date = DateTime.Now;
-            if (date.Month <= 6) sid += "v";
-            else sid += "h";
-            sid += date.Year.ToString().Substring(2);
-            sid += FirstName.ToString().Substring(0, 3) + LastName.ToString().Substring(0, 2);
-            StudentID = sid;
+            if (date.Month <= 6) sID += "v";
+            else sID += "h";
+            sID += date.Year.ToString().Substring(2);
+            sID += FirstName.ToString().Substring(0, 3) + LastName.ToString().Substring(0, 2);
+
+            return sID;
         }
     }
 }
