ac330f31c0991bd87136a9de67c7c81c896250cf
timstolpe <105752665+timstolpe@users.noreply.github.com>
2023-04-18 10:46:33 +0200

    Lab2 hashtablearray

--- Lab2/Lab2/HashtableArray.cs
+++ Lab2/Lab2/HashtableArray.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,18 @@ namespace Lab2
         private const int _defaultCapacity = 8;
         private const float _loadFactorThreshold = 0.7f;
 
-        private KeyValuePair<K, V>[] _hashTable;
-        private bool[] _isOccupied;
-
         private int _count = 0;
         private int _capacity = _defaultCapacity;
+        private int _bucketCapacity = _defaultCapacity / 2;
+
+        private KeyValuePair<K, V>[,] _hashTable; 
+        private bool[,] _isOccupied;
         
 
         public HashTableArray() 
         {
-            _hashTable = new KeyValuePair<K, V>[_capacity];
-            _isOccupied = new bool[_capacity];
+            _hashTable = new KeyValuePair<K, V>[_capacity, _capacity]; // I am confusion, men jag tror att det blir såhär om man ska använda arrays som buckets?
+            _isOccupied = new bool[_capacity, _capacity];
         }
 
         public bool Add(K key, V value)
@@ -37,44 +39,49 @@ namespace Lab2
             }
 
             // Calculate index using hash function
-            int hash = key.GetHashCode() % _capacity;
-            KeyValuePair<K,V> pair = _hashTable[hash];
+            int HashIndex = key.GetHashCode() % _capacity;
 
-            // If index isn't empty
-            if (pair != null) 
+            for (int i = 0; i < _bucketCapacity; i++) 
             {
-                K indexKey = pair.GetKey();
-                
-                if (indexKey.Equals(key))  // If the keys match
+                KeyValuePair<K, V> pair = _hashTable[i, HashIndex];
+
+                if (pair != null) // If index isn't empty
                 {
-                    // Set the value of the pair to the new passed in value.
-                    pair.SetValue(value);
+                    K indexKey = pair.GetKey();
+
+                    if (indexKey.Equals(key))  // If the keys match
+                    {
+                        // Set the value of the pair to the new passed in value.
+                        pair.SetValue(value);
+                        return true;
+                    }
+                    else // If they don't match, then perform conflict resolution
+                    {
+
+                        // Implement strategy to combat conflicts
+                        return false;
+                    }
+                }
+                else // If index is empty
+                {
+                    // Add Value
+                    KeyValuePair<K, V> newPair = new KeyValuePair<K, V>(key, value);
+                    _hashTable[i, HashIndex] = newPair;
+                    _isOccupied[i, HashIndex] = true;
+                    // Increment count
+                    _count++;
                     return true;
                 }
-                else // If they don't match, then perform conflict resolution
-                {
-                    // Implement strategy to combat conflicts
-                    return false;
-                }
             }
-            else // If index is empty
-            {
-                // Add Value
-                KeyValuePair<K,V> newPair = new KeyValuePair<K,V>(key, value);
-                _hashTable[hash] = newPair;
-                _isOccupied[hash] = true;
-                // Increment count
-                _count++;
-                return true;
-            }
+            
         }
 
         public void Clear()
         {
             throw new NotImplementedException();
             // WIP
-            _hashTable = new KeyValuePair<K, V>[_defaultCapacity];
-            _isOccupied = new bool[_defaultCapacity];
+            _hashTable = new KeyValuePair<K, V>[_defaultCapacity, _defaultCapacity];
+            _isOccupied = new bool[_defaultCapacity, _defaultCapacity];
             _capacity = _defaultCapacity;
             _count = 0;
         }
@@ -82,23 +89,26 @@ namespace Lab2
         public bool ContainsKey(K key)
         {
             throw new NotImplementedException();
-            for (int i = 0; i <= Size(); i++)
+            int HashIndex = -1;
+
+            if (key != null)
             {
-                KeyValuePair<K, V> pair = _hashTable[i];
+                HashIndex = key.GetHashCode() % _capacity;
+            }
+            else
+            {
+                throw new NullKeyException();
+            }
+
+            for (int i = 0; i < _bucketCapacity; i++) 
+            {
+                KeyValuePair<K, V> pair = _hashTable[i, HashIndex];
                 K indexKey = default;
 
                 if (pair != null) indexKey = pair.GetKey();
-                
-                if (indexKey.Equals(key))
-                {
-                    return true;
-                }
-                else
-                {
-                    // Lägg till kod för att kolla chaining
-                }
 
-                
+                if (indexKey.Equals(key)) return true;
+
             }
 
             return false;
@@ -106,24 +116,21 @@ namespace Lab2
 
         public bool ContainsValue(V value)
         {
+            // Det går säkert att göra den här funktionen mer effektiv
             throw new NotImplementedException();
-            for (int i = 0; i <= Size(); i++)
+            for (int i = 0; i < Size(); i++)
             {
-                KeyValuePair<K, V> pair = _hashTable[i];
-                V indexValue = default;
+                for (int j = 0; j < _bucketCapacity; j++) 
+                {
+                    KeyValuePair<K, V> pair = _hashTable[j, i];
+                    V indexValue = default;
 
-                if (pair != null) indexValue = Get(pair.GetKey());
+                    if (pair != null) indexValue = Get(pair.GetKey());
+
+                    if (indexValue.Equals(value)) return true;
+
+                }
                
-                if (indexValue.Equals(value))
-                {
-                    return true;
-                }
-                else
-                {
-                    // Lägg till kod för att kolla chaining
-                }
-
-                
             }
 
             return false;
@@ -139,15 +146,18 @@ namespace Lab2
             }
             else
             {
-                // Throw some kind of null key error
+                throw new NullKeyException();
             }
             
-            KeyValuePair<K, V> pair = _hashTable[HashIndex];
-
-            if (pair != null) 
+            for (int i = 0; i < _bucketCapacity; i++)
             {
-                K indexKey = pair.GetKey();
-                if (indexKey != null && indexKey.Equals(key)) return pair.GetValue();
+                KeyValuePair<K, V> pair = _hashTable[i, HashIndex];
+
+                if (pair != null)
+                {
+                    K indexKey = pair.GetKey();
+                    if (indexKey != null && indexKey.Equals(key)) return pair.GetValue();
+                }
             }
 
             return default;
@@ -161,6 +171,35 @@ namespace Lab2
         public bool Remove(K key)
         {
             throw new NotImplementedException();
+            int HashIndex = -1;
+
+            if (key != null)
+            {
+                HashIndex = key.GetHashCode() % _capacity;
+            }
+            else
+            {
+                throw new NullKeyException();
+            }
+
+            for (int i = 0; i < _bucketCapacity; i++)
+            {
+                KeyValuePair<K, V> pair = _hashTable[i, HashIndex];
+
+                if (pair != null)
+                {
+                    K indexKey = pair.GetKey();
+                    if (indexKey != null && indexKey.Equals(key))
+                    {
+                        _hashTable[i, HashIndex] = null;
+                        _isOccupied[i, HashIndex] = false;
+                        _count--;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
         public int Size()
---
+++ Lab2/Lab2/NullKeyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    [Serializable]
+    internal class NullKeyException : Exception
+    {
+        public NullKeyException() : base("Error: Key is null!") { }
+    }
+}
