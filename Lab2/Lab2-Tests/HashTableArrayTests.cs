ecc7abf49ee1550d00cdb5f8da8e2e8957ed99c7
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2023-04-20 12:04:32 +0200

    Basic teststruktur array

--- Lab2/Lab2-Tests/HashTableArrayTests.cs
+++ Lab2/Lab2-Tests/HashTableArrayTests.cs
@@ -1,22 +1,75 @@
+using Lab2;
+
 namespace Lab2_Tests;
 
 [TestClass]
 public class HashTableArrayTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestAdd()
+    {
+        // Test if program can handle empty strings
+        HashTableArray<string, Student> map = new HashTableArray<string, Student>();
+        Student emptyLastname = new Student("Markus","");
+        Student enmptyFirstname = new Student("", "Stolpe");
+        Student emptyName = new Student("", "");
+
+
+    }
+
+    [TestMethod]
+    public void TestGet()
     {
 
     }
 
     [TestMethod]
-    public void TestMethod2()
+    public void TestRemove()
     {
 
     }
-
     [TestMethod]
-    public void TestMethod3()
+    public void TestContainsValue()
+    {
+
+    }
+    [TestMethod]
+    public void TestContainsKey()
+    {
+
+    }
+    [TestMethod]
+    public void TestClear()
+    {
+
+    }
+    [TestMethod]
+    public void TestIsEmpty()
+    {
+
+    }
+    [TestMethod]
+    public void TestTotalCount()
+    {
+
+    }
+    [TestMethod]
+    public void TestCount()
+    {
+
+    }
+    [TestMethod]
+    public void TestBucketCount()
+    {
+
+    }
+    [TestMethod]
+    public void TestCapacity()
+    {
+
+    }
+    [TestMethod]
+    public void TestResize()
     {
 
     }
