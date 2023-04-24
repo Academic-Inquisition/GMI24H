b61be2c79201a3d29ea535b7bce34c8325e60b86
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2023-04-24 12:40:37 +0200

    Logic for Multitables
    Logic to print multiplicationtables 1-9

--- Lab3/MultiplicationTable.cs
+++ Lab3/MultiplicationTable.cs
@@ -8,9 +8,24 @@ namespace Lab3
 {
     internal class MultiplicationTable
     {
-        public MultiplicationTable() 
+        public MultiplicationTable(int table) 
         { 
-        
+            
+                /*Pseudo kod för denna uppgift skulle kunna se ut ungefär såhär. 
+                Metoden kommer inte behöva något värde inmatat för att den ska skriva ut multipliktionstabellerna från 1-9 oavsätt.
+                Det finns ingen variation för output.
+                Så hur den skulle kunna se ut.
+                For (i in range 1-10){
+                    for(j in range 1-10){
+                        skriv ij 
+                
+                        }
+                }
+                */
+        }
+         public int MultiplicationTableFunc( int _multiTable)
+        {
+            return _multiTable;
         }
     }
 }
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -7,6 +7,19 @@
             MinimumArrayValue minValue = new MinimumArrayValue(10);
 
             Console.WriteLine($"Den aktuella arrayen är: {minValue.GetArray()}.\nDet minsta värdet är {minValue.Run()}");
+
+            // Logic to print all multiplicationtables
+            for (int i = 1; i < 10; i++)// make the tables 1-9
+                    {
+                        Console.WriteLine("\n{0}:ans-tabell\n", i);
+
+                        for (int j = 1; j <= 10; j++) // Multiplý each number 1-10
+                        {
+                            int k = i * j;
+                            Console.WriteLine("{0}x{1}={2}", i, j, k);
+                        }
+                    }
+            
         }
     }
 }
