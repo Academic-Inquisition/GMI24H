2d411cf3cfb7c1d889036afb97e5bb9b420afa18
h21tisto <89834645+h21tisto@users.noreply.github.com>
2023-04-28 13:45:35 +0200

    Update Program.cs

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -7,34 +7,34 @@ namespace Lab3
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-            MinimumArrayValue minValue = new MinimumArrayValue(10);
+            MinimumArrayValue minValue = new MinimumArrayValue(100);
             
             sw.Start();
             int minimum_value = minValue.Run();
             sw.Stop();
 
             Console.WriteLine($"Finished 'MinimumArrayValue.Run()' in: {sw.Elapsed} seconds.");
-            Console.WriteLine($"Den aktuella arrayen är: {minValue.GetArray()}.\nDet minsta värdet är {minimum_value}");
-            
-            Console.WriteLine();
+            Console.WriteLine($"Den aktuella arrayen är: {minValue.GetArray()}.\nDet minsta värdet är {minimum_value}\n");
+
 
             sw = new Stopwatch();
+
             sw.Start();
             MultiplicationTable.CreateMultiplicationTable();
             sw.Stop();
-            Console.WriteLine($"Finished 'CreateMultiplicationTable' in: {sw.Elapsed} seconds.");
 
-            Console.WriteLine();
+            Console.WriteLine($"Finished 'CreateMultiplicationTable' in: {sw.Elapsed} seconds.\n");
+
 
             string str = "A man, a plan, a canal, Panama!";
             sw = new Stopwatch();
+
             sw.Start();
             string isPalindrome = PalindromeAlgorithm.IsPalindrome(str) ? "Yes" : "No";
             sw.Stop();
+
             Console.WriteLine($"Finished 'IsPalindrome' in: {sw.Elapsed} seconds.");
             Console.WriteLine($"Is '{str}' a Palindrome?: Judges says {isPalindrome}");
-
-            Console.ReadLine();
         }
     }
 }
