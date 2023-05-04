0a185c3e438955726fd7a7d955fcc793abcdd293
Lanse505 <simon_stalnabb@hotmail.com>
2023-05-04 13:33:17 +0200

    Push

---
+++ Lab5/GMI24H_Labb5_Grupp_ERTGRUPPNR/sort/Bubble_Sort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Bubble_Sort<T>
+{
+
+    public Sort(T[] array)
+    {
+        bool not_sorted = true;
+        while (not_sorted)
+        {
+            not_sorted = false;
+            
+        }
+    }
+}
---
+++ Lab5/GMI24H_Labb5_Grupp_ERTGRUPPNR/sort/Heap_Sort.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public class Heap_Sort
+{
+	
+}
