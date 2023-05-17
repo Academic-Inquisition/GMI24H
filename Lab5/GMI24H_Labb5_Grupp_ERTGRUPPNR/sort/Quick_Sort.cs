2ef0bba7365c327508e012dfc77fb9c53b18e93d
h21tisto <89834645+h21tisto@users.noreply.github.com>
2023-05-17 09:05:21 +0200

    Lab5

--- Lab5/GMI24H_Labb5_Grupp_ERTGRUPPNR/sort/Quick_Sort.cs
+++ Lab5/GMI24H_Labb5_Grupp_ERTGRUPPNR/sort/Quick_Sort.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.ComponentModel;
 
 public class Quick_Sort
 {
 	public Quick_Sort() {}
 
-    public void Sort(int[] array)
+    public void Sort(int[] array, int start, int end)
     {
+        int divider = start;
 
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < divider) 
+            {
+                
+            }
+
+            if (array[i] >= divider)
+            {
+
+            }
+        }
     }
 }
