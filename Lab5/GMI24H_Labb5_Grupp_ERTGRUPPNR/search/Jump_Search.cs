7d09c8aca5bce9b600744f2aa039deafde47fcf1
timstolpe <105752665+timstolpe@users.noreply.github.com>
2023-06-01 15:04:32 +0200

    Update Jump_Search.cs

--- Lab5/GMI24H_Labb5_Grupp_ERTGRUPPNR/search/Jump_Search.cs
+++ Lab5/GMI24H_Labb5_Grupp_ERTGRUPPNR/search/Jump_Search.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Jump_Search
 {
@@ -7,19 +8,20 @@ public class Jump_Search
     public int Search(int[] array, int target)
     {
         new Insertion_Sort().Sort(array);
-        int step = (int) Math.Floor(Math.Sqrt(array.Length));
+        int step = Floor(Sqrt(array.Length));
         int prev = 0;
-        while (array[Math.Min(step, array.Length) - 1] < target)
+
+        while (array[Min(step, array.Length) - 1] < target)
         {
             prev = step;
-            step += (int) Math.Floor(Math.Sqrt(array.Length - prev));
+            step += Floor(Sqrt(array.Length - prev));
             if (prev >= array.Length)
                 return -1;
         }
         while (array[prev] < target)
         {
             prev++;
-            if (prev == Math.Min(step, array.Length))
+            if (prev == Min(step, array.Length))
                 return -1;
         }
         if (array[prev] == target)
@@ -27,4 +29,37 @@ public class Jump_Search
         else
             return -1;
     }
+
+    private int Min(float x, float y)
+    {
+        return (int)(x < y ? x : y);
+    }
+
+    private int Floor(double x)
+    {
+        return (int)x;
+    }
+
+    // Kom ihåg att referera till källan: 
+    // https://www.tutorialspoint.com/Babylonian-method-to-find-the-square-root
+
+    private double Sqrt(float x)
+    {
+        float y = 1; //initial guess as 1
+        float number = x;
+        float precision = 0.000001f;           //the result is correct upto 0.000001
+
+        while (Abs(x - y) / Abs(x) > precision)
+        {
+            x = (x + y) / 2;
+            y = number / x;
+        }
+        return x;
+    }
+
+    private double Abs(double  x) 
+    {
+        return (x < 0) ? -x : x;
+    }
+
 }
